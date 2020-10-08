2aafdbc207669b87ebaa6a2d8a871ad6e2d8822b
Brandon Corman <brandon.cormang@utem.cl>
2020-10-08 16:54:46 -0300

    Se agregan controladores

---
+++ app/Http/Controllers/GrafoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GrafoController extends Controller
+{
+    public function __invoke(){
+        return 'Grafo Dirigido';
+    }
+}
