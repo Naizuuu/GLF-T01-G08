d53a8589aea6b1f6c5be0c6e13f3fee164c08c3d
Brandon Corman <brandon.cormang@utem.cl>
2020-10-13 22:25:03 -0300

    Edición de grafo

--- resources/views/grafo-dirigido.blade.php
+++ resources/views/grafo-dirigido.blade.php
@@ -3,7 +3,21 @@
 @section('title', 'Grafo dirigido')
 
 @section('content')
-    <h1>Grafo dirigido</h1>
-    <input type="text" name="" value="">
-    <input type="submit" name="" value="Ingresar">
+    <form class="ingresarGrafo" action="grafo-dirigido" method="get">
+        <h1>Grafo dirigido</h1>
+        <input type="text" name="grafo" value="">
+        <input type="submit" name="" value="Ingresar">
+    </form>
+    
 @endsection
+@php
+    if(isset($_GET['grafo']) && is_string($_GET['grafo']))
+    {
+        $grafo = $_GET ['grafo'];
+        echo $grafo;
+    }
+    else
+    {
+        echo "No es string";
+    }
+@endphp
