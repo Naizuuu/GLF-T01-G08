91e534fdaccec9fb20b19d944a167dd8335292c1
Sebastián <sebazmo@gmail.com>
2020-10-16 19:06:17 -0300

    Se arreglaron uno errores entre otras cosas.
    * Se agregó otro formulario (que está sirviendo)
    * Se mantiene comentado la siguiente parte mientras se trabaja en ella

--- resources/views/grafos/grafo-simple.blade.php
+++ resources/views/grafos/grafo-simple.blade.php
@@ -9,50 +9,82 @@
 @include('layouts.partials.crear-cont')
 
     <div class="container">
-        <form style="margin-top: 5%" class="ingresarGrafo" action="grafo-simple" method="get">
+        <form style="margin-top: 5%" class="ingresarGrafo" method="get">
             <hr class="my-4">
             <h1 class="display-5">Grafo Simple</h1>
             <div class="form-group" style="margin-top: 2%">
-                <label for="texto_de_ejemplo">Vértices</label>
-                <input type="text" class="form-control" name="verticesGrafoSimple" aria-describedby="textoGrafoSimple" title="Debe ingresar los vértices como el ejemplo: a,b,c" pattern="^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$" placeholder="Ingrese los vértices separados por comas. (Ej: a,b,c,d)" required>
+                <label for="verticesGrafoSimple">Vértices</label>
+                <input type="text" class="form-control" name="verticesGrafoSimple" title="Debe ingresar los vértices como el ejemplo: a,b,c" pattern="^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$" placeholder="Ingrese los vértices separados por comas. (Ej: a,b,c,d)" autocomplete="off" required>
+            </div>
+            @php
+                if(isset($_GET['verticesGrafoSimple']) && is_string('verticesGrafoSimple'))
+                {
+                    $grafosimple = new Grafo($_GET['verticesGrafoSimple']);
+                }
+            @endphp
+        </form>
 
-                @php
-                    if(isset($_GET['verticesGrafosSimple']) && is_string('verticesGrafosSimple')){
-                        $grafosimple = new Grafo($_GET['verticesGrafoSimple']);
-                    }
-                @endphp
-      
-@php
-for($i = 0; $i < 5; $i++)
-{
-    echo <<< HTML
-    <div class="row">
-        <div class="col-sm">
-            <div class="input-group" style="margin-top: 2%">
-                <select class="custom-select" id="inputGroupSelect02">
-                <option selected>a</option>
-                <option value="1">b</option>
-                <option value="2">c</option>
-                <option value="3">d</option>
-                </select>
+        <form style="margin-top: 0%" class="ingresarGrafo" method="get">  
+            <div class="form-group" style="margin-top: 2%"> 
+                <label for="cantidadDeAristas">Cantidad de Aristas</label>
+                <input type="number" class="form-control" name="cantidadDeAristas" title="Debe ingresar la cantidad de aristas en el grafo." placeholder="Ingrese la cantidad de aristas." min="0" autocomplete="off">         
             </div>
-        </div>
-        <div class="col-sm">
-            <div class="input-group" style="margin-top: 2%">
-                <select class="custom-select" id="inputGroupSelect02">
-                <option selected>a</option>
-                <option value="1">b</option>
-                <option value="2">c</option>
-                <option value="3">d</option>
-                </select>
-            </div>
-        </div>
-    </div>
-HTML;
-}
-@endphp
-            </div>
+            @php
+                if(isset($_GET['cantidadDeAristas']))
+                {
+                    $cantidad_de_aristas = (int)$_GET['cantidadDeAristas'];
+                    var_dump($cantidad_de_aristas);
+                }
+            @endphp
         </form>
     </div>
+
+        {{-- <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text">First and last name</span>
+            </div>
+            <input type="text" aria-label="First name" class="form-control">
+            <input type="text" aria-label="Last name" class="form-control">
+        </div> --}}
+    
+            {{-- @php
+                if(isset($_GET['cantidadDeAristas']) && is_numeric('cantidadDeAristas'))
+                {
+                    $cantidad_de_aristas = $_GET['cantidadDeAristas'];
+                    var_dump($cantidad_de_aristas);
+                    
+                    echo 'div class="form-group" style="margin-top: 2%">';
+                    echo '<label for="texto_de_ejemplo">Seleccione las uniones.</label>';
+                    
+                    for($i = 0; $i < $cantidad_de_aristas; $i++)
+                    {
+                        echo <<< 'HTML'
+                            <div class="row">
+                                <div class="col-sm">
+                                    <div class="input-group" style="margin-top: 2%">
+                                        <select class="custom-select" id="inputGroupSelect02">
+                                        <option selected>a</option>
+                                        <option value="1">b</option>
+                                        <option value="2">c</option>
+                                        <option value="3">d</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-sm">
+                                    <div class="input-group" style="margin-top: 2%">
+                                        <select class="custom-select" id="inputGroupSelect02">
+                                        <option selected>a</option>
+                                        <option value="1">b</option>
+                                        <option value="2">c</option>
+                                        <option value="3">d</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+HTML;
+                    }
+                }
+            @endphp --}}
+        {{-- </form> --}}
     
 @endsection
