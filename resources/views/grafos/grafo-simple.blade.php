cd80dc9bea139861401b5f917f8525fd7c22aa43
Sebastián <sebazmo@gmail.com>
2020-10-16 16:22:04 -0300

    Se agregó una lista (en construccion)

--- resources/views/grafos/grafo-simple.blade.php
+++ resources/views/grafos/grafo-simple.blade.php
@@ -10,23 +10,31 @@
 
     <div class="container">
         <form style="margin-top: 5%" class="ingresarGrafo" action="grafo-simple" method="get">
-           
-           
-            {{-- <h1 class="display-4">Grafo simple</h1>
-            <input type="text" name="grafo" value="" placeholder="Ingrese el grafo asi: akjnkjcnsd">
-            <input type="submit" name="" value="Ingresar">
-            <output type = "grafo" name=""></output> --}}
             <hr class="my-4">
             <h1 class="display-5">Grafo Simple</h1>
-            <div class="form-group">
+            <div class="form-group" style="margin-top: 2%">
                 <label for="texto_de_ejemplo">Vértices</label>
                 <input type="text" class="form-control" name="verticesGrafoSimple" aria-describedby="textoGrafoSimple" title="Debe ingresar los vértices como el ejemplo: a,b,c" pattern="^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$" placeholder="Ingrese los vértices separados por comas. (Ej: a,b,c,d)" required>
-                {{-- <small id="textoGrafoSimple" class="form-text text-muted">Ponte vio.</small> --}}
+
                 @php
                     if(isset($_GET['verticesGrafosSimple']) && is_string('verticesGrafosSimple')){
                         $grafosimple = new Grafo($_GET['verticesGrafoSimple']);
                     }
                 @endphp
+
+            <div class="container-sm">
+                <div class="row">
+                    <div class="input-group" style="margin-top: 2%">
+                        <select class="custom-select" id="inputGroupSelect02">
+                        <option selected>a</option>
+                        <option value="1">b</option>
+                        <option value="2">c</option>
+                        <option value="3">d</option>
+                        </select>
+                    </div>
+                </div>        
+            </div>
+
             </div>
         </form>
     </div>
