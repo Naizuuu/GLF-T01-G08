5d2e6cac226c3002dd9a831adbf549e42de8e0b4
Brandon Corman <brandon.cormang@utem.cl>
2020-10-17 17:21:28 -0300

    Se modifican las clases

--- resources/views/grafos/clases_grafos.php
+++ resources/views/grafos/clases_grafos.php
@@ -1,7 +1,8 @@
 <?php
     class Grafo {
-        public $matriz_de_adyacencias = [];
-        public $vertices = [];
+        protected $matriz_de_adyacencias = [];
+        protected $vertices = [];
+        protected $etiquetas = [];
         public function __construct($texto_de_vertices)
         {
             $posicion = 0;
@@ -13,21 +14,22 @@
             foreach($this->vertices as $vertice_i){
                 $columnas = [];
                 foreach($this->vertices as $vertice_j){
-                    $columnas[$vertice_j] = 0;
+                    $columnas[$vertice_j] = [0,0];
                 }
                 $this->matriz_de_adyacencias[$vertice_i] = $columnas;
             }
         }
-        public function llenar_la_matriz_de_adyacencias ($texto_de_adyacencias) //'a,b;c,d;a,d'
+        public function llenar_la_matriz_de_adyacencias ($texto_de_adyacencias) //'a,0,b';'c,6,d';'a,5,d'
         {
             $adyacencias_separadas_por_punto_y_coma = explode(";", $texto_de_adyacencias);//'a,b','c,d','a,d'
             foreach($adyacencias_separadas_por_punto_y_coma as $adyacencia){
-                $adyacencia_separada_por_coma = explode(",", $adyacencia);//'a','b'
+                $adyacencia_separada_por_coma = explode(",", $adyacencia);//'a','0','b'
                 $vertice1=$adyacencia_separada_por_coma[0];//'a'
+                $etiqueta=(int)$adyacencia_separada_por_coma[1];//'0'
                 $vertice2=$adyacencia_separada_por_coma[1];//'b'
                 $posicion_i=array_search($vertice1, $this->matriz_de_adyacencias);//Posición de fila de 'a' en la matriz
                 $posicion_j=array_search($vertice2, $this->matriz_de_adyacencias[$vertice1]);//Posición de columna de 'b' en la matriz
-                $this->matriz_de_adyacencias[$posicion_i][$posicion_j] = 1;//Se reemplaza por 1 esa posición
+                $this->matriz_de_adyacencias[$posicion_i][$posicion_j] = [1,$etiqueta];//Se reemplaza por 1 esa posición
             }
         }
         public function matriz_identidad(){
@@ -35,24 +37,25 @@
             foreach($matriz_identidad as $posicion_i){
                 foreach($posicion_i as $posicion_j){
                     if($posicion_i==$posicion_j){
-                        $matriz_identidad[$posicion_i][$posicion_j]=1;
+                        $matriz_identidad[$posicion_i][$posicion_j]=[1,0];
                     }else{
-                        $matriz_identidad[$posicion_i][$posicion_j]=0;
+                        $matriz_identidad[$posicion_i][$posicion_j]=[0,0];
                     }
                 }
             }
             return $matriz_identidad;
         }
-        public function suma_de_matrices($matriz_1, $matriz_2){
+        protected function suma_de_matrices($matriz_1, $matriz_2){
             $resultado = [];
             foreach($matriz_1 as $posicion_i){
                 foreach($posicion_i as $posicion_j){
-                    $resultado[$posicion_i][$posicion_j] = $matriz_1[$posicion_i][$posicion_j] + $matriz_2[$posicion_i][$posicion_j];
+                    $array_auxiliar=[0,0];
+                    $resultado[$posicion_i][$posicion_j] = $matriz_1[$posicion_i][$posicion_j] + $matriz_2[$posicion_i][$posicion_j][0];
                 }
             }
             return $resultado;
         }
-        public function multiplicacion_de_matrices($matriz_1, $matriz_2){
+        protected function multiplicacion_de_matrices($matriz_1, $matriz_2){
             $resultado = [];
             foreach ($matriz_1 as $posicion_i){
                 foreach($posicion_i as $posicion_j){
@@ -64,14 +67,14 @@
             }
             return $resultado;
         }
-        public function tamano_de_la_matriz($matriz){
+        protected function tamano_de_la_matriz($matriz){
             $contador = 0;
             foreach($matriz as $posiciones){
                 $contador+=1;
             }
             return $contador;
         }
-        public function potencia_de_matriz($matriz, $exponente){
+        protected function potencia_de_matriz($matriz, $exponente){
             $resultado = $this->matriz_identidad();
             for($i = 1; $i<=$exponente; $i++){
                 $resultado = $this->multiplicacion_de_matrices($resultado, $matriz);
